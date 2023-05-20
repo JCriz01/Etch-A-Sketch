ffa4540bf2d525cdee9dca94bb37f707f677e463
Jesse <skydoesgta802@gmail.com>
2023-05-20 11:07:04 -0400

    Made a fully functional button event listener where the user is able to change the amount of grids the container will show without changing the amount of pixels the container needs,

--- JS.js
+++ JS.js
@@ -1,6 +1,7 @@
 
 //parent element
 const parentElement=document.getElementsByClassName('container')[0];
+let containerSizeEdge=736;
 
 
 //function that will create the hover effect everytime a user hovers over an individual grid
@@ -10,44 +11,74 @@ function Hover(list){
     for(let i=0; i < list.length; i++){
 
         list[i].addEventListener('mouseenter',()=> {
-            list[i].classList.remove('grids');
-            list[i].classList.add('entered');
+            list[i].style.backgroundColor=`aqua`;
         });
 
     }
 }
 
-
-
-//main function that will maintain everything, etc.
-function MainFunction(element){
-
-
+//function to take size of grid and create grid layout.
+function CreateGrid(size){
     //individual div element
     const grid=document.createElement('div');
 
-    const button=document.createElement('button');
-    button.textContent=`Change grid size?`;
-    parentElement.parentNode.appendChild(button);
-    button.addEventListener('click',()=>{
-        let gridSize=prompt("Enter the length of the new grid(under 100): ");
-    });
+    let gridSize=size*size;
+    let gridDivSize=(736/size)-2;
+    
 
     //for loop that creates and adds a class to each div
-    for(let i=0; i < 256; i++){
+    for(let i=0; i < gridSize; i++){
         const grid=document.createElement('div');
         grid.classList.add('grids');
-        element.appendChild(grid);//appending to parent element
-
+        grid.style.width=`${gridDivSize}px`;
+        grid.style.height=`${gridDivSize}px`;
+        parentElement.appendChild(grid);//appending to parent element
+        
     }
-
-    //variable that contains a HTMLCollection list for each div that represents a grid.
-    const gridlist=parentElement.children;
-
-    Hover(gridlist);//calling Hover function to create hover effect for the user.
 
 }
 
-MainFunction(parentElement);//calling main function
+function DeleteGrid(list){
 
+    
+    //getting the size of the HTMLCollection
+    let gridSize=parentElement.children.length;
+    console.log(gridSize);
+    for(let i=(gridSize-1); i >=0 ;i--){
+        parentElement.removeChild(list[i]);
+    }
+
+
+}
+
+CreateGrid(16);
+
+//variable that contains a HTMLCollection list for each div that represents a grid.
+let gridlist=parentElement.children;
+
+
+const button=document.createElement('button');
+button.textContent=`Change grid size?`;
+button.classList.add('button-class');//adding class to button
+parentElement.parentNode.appendChild(button);
+
+button.addEventListener('click',()=>{
+    let size=prompt("Enter the length of the new grid(under 100): ");
+    
+    if(size < 100 && size > 0){
+
+        DeleteGrid(gridlist);
+
+    
+        CreateGrid(size);
+        gridlist=parentElement.children;
+        Hover(gridlist);
+    }
+    else{
+        alert("Error. Invalid entry.");
+    }
+});
+
+
+Hover(gridlist);//calling Hover function to create hover effect for the user.
 
