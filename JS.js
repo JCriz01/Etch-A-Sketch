1af6d08a61083e4e600ed9e9c0bbb0141bfb07a3
Jesse <skydoesgta802@gmail.com>
2023-09-08 19:46:55 -0400

    refactoring UI and adding touch API implementation

--- JS.js
+++ JS.js
@@ -1,6 +1,38 @@
+const myStyleSheet=document.styleSheets[0];
+
+console.log(myStyleSheet);
+
 
 //parent element
 const parentElement=document.querySelector('.container');
+let currentColor='#151515';//default black color
+
+function changeCSSDivPropertyColor(currColor){
+    for(let i=0; i < myStyleSheet.cssRules.length; i++){
+        let rule=myStyleSheet.cssRules[i]
+
+        if(rule.selectorText === '.entered'){
+            rule.style.backgroundColor=currColor;
+        }
+    }
+}
+function createRandomColor(){
+    const randomBtnElem=document.querySelector('.random');
+
+    function randomRGB(){
+        const red=Math.floor(Math.random() * 255)+1;
+        const green=Math.floor(Math.random() * 255)+1;
+        const blue=Math.floor(Math.random() * 255)+1;
+
+        return `rgb(${red},${green},${blue})`;
+    }
+    randomBtnElem.addEventListener('click',()=>{
+        let randomColor=randomRGB()
+        randomBtnElem.style.backgroundColor=randomColor;
+        //changeCSSDivPropertyColor(randomColor);
+        currentColor=randomColor;
+    });
+}
 
 function gridEvent(){
     let isMouseDown=false;
@@ -11,14 +43,18 @@ function gridEvent(){
         if(target.classList.value==='grids'){
             isMouseDown=true;
             target.classList.add('entered');
+            target.style.backgroundColor=currentColor;
+
             activeDiv=target;
         }
     })
 
     parentElement.addEventListener('mousemove',(Event)=>{
         let target=Event.target;
-        if(isMouseDown && activeDiv){
+        if(isMouseDown && activeDiv && target.classList.value==='grids'){
             target.classList.add('entered');
+            target.style.backgroundColor=currentColor;
+
         }
     })
 
@@ -41,18 +77,56 @@ function createElements(element){
     }
 }
 
+function gridEventsTouchScreen(){
+
+    let isFingerDown=false;
+    let activeDiv=null;
+
+    parentElement.addEventListener('touchstart',(Event)=>{
+        Event.preventDefault();
+        let target=Event.target;
+
+        if(target.classList.value==='grids'){
+            isFingerDown=true;
+            target.classList.add('entered');
+            target.style.backgroundColor=currentColor;
+            activeDiv=target;
+        }
+    });
+    parentElement.addEventListener('touchmove',(Event)=>{
+        Event.preventDefault();
+
+        let target=Event.target;
+        console.log(target);
+        if(isFingerDown && activeDiv){
+            target.classList.add('entered');
+        }
+    });
+    parentElement.addEventListener('touchend',(Event)=>{
+        //Event.preventDefault();
+        isFingerDown=false;
+        activeDiv=null;
+    })
+}
+
 
 
 //main function that will maintain everything, etc.
 function MainFunction(element){
 
+    createRandomColor();
 
     //creating grids
     createElements(element);
     const button=document.createElement('button');
     //button.textContent=`Change grid size?`;
     //parentElement.parentNode.appendChild(button);
-    gridEvent()
+    if(/Mobi/.test(navigator.userAgent))
+        gridEventsTouchScreen();
+    else
+        gridEvent();
+
+
     button.addEventListener('click',()=>{
         let gridSize=prompt("Enter the length of the new grid(under 100): ");
     });
@@ -73,30 +147,5 @@ function DeleteGrid(list){
 }
 
 
-//variable that contains a HTMLCollection list for each div that represents a grid.
-let gridlist=parentElement.children;
-
-
-const button=document.createElement('button');
-button.textContent=`Change grid size?`;
-button.classList.add('button-class');//adding class to button
-parentElement.parentNode.appendChild(button);
-
-button.addEventListener('click',()=>{
-    let size=prompt("Enter the length of the new grid(under 100): ");
-    
-    if(size < 100 && size > 0){
-
-        DeleteGrid(gridlist);
-
-    
-        CreateGrid(size);
-        gridlist=parentElement.children;
-        Hover(gridlist);
-    }
-    else{
-        alert("Error. Invalid entry.");
-    }
-});
 
 MainFunction(parentElement);
